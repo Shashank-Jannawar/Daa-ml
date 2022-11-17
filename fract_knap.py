c61f327d5dc03a692e3c3cea7c651aa4b267f18c
Shashank S Jannawar <72351961+Shashank-Jannawar@users.noreply.github.com>
2022-11-17 21:58:42 +0530

    Update fract_knap.py

--- fract_knap.py
+++ fract_knap.py
@@ -1,26 +1,27 @@
-def fib(n):
-    if n==0 or n==1:
-        return n
-    else:
-        return fib(n-1)+fib(n-2)
-        
-n=int(input("enter the number:-"))
-print(fib(n))
+item=[10,20,30]
+profit=[60,100,120]
 
-def fib(n):
-    if n == 0 or n ==1:
-        return n
+c=120
+
+arr=[]
+for i in range(0,len(item)):
+    it=item[i]
+    pt=profit[i]
+    pkg=pt/it
+    arr.append([it,pt,pkg])
+
+arr.sort(key=lambda x:x[2])
+arr=arr[::-1]
+
+i=0
+profit=0
+while(c>0):
+    if arr[i][0]<=c:
+        c=c-arr[i][0] #positive
+        profit+=arr[i][1]
     else:
-        i=0
-        num1=0
-        num2=1
-        while(i<n-1):
-            i+=1
-            sum = num1+num2
-            num1=num2
-            num2=sum
-        return sum
-            
-n=int(input("enter the number:-"))
-print(fib(n))
-            
+        profit+=c*arr[i][2]
+        c=0
+    i+=1
+
+print(profit)
